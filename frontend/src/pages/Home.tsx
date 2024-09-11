d77d2f3b3879ca9a46f3bb4e19a27d70377ed784
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-09-11 17:42:05 +0800

    Update HomePage and NavBar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,20 +3,24 @@ import cube from "../assets/3dCube.gif"
 
 const Home = () => {
     return (
-        <section className='hero-section'>
-            <div className="left-hero-section">
-                <div className='h1-container'>
-                    <h1>Learn and</h1>
-                    <h1 className='h1-sub'> Conquer </h1>
+        <>
+            <section className='hero-section'>
+                <div className="left-hero-section">
+                    <div className='h1-container'>
+                        <h1>Learn and</h1>
+                        <h1 className='h1-sub'> Conquer </h1>
+                    </div>
+                    <h1> Data Structures and Algorithms</h1>
+                    <button className='start-btn'>START NOW!</button>
                 </div>
-                <h1> Data Structures and Algorithms</h1>
-                <button className='start-btn'>START NOW!</button>
-            </div>
-            <div className="right-hero-section">
-                <h1 className='rh-title'>CODE & CONQUER</h1>
-                <img src={cube} alt="3dCube" className='rh-img'/>
-            </div>
-        </section>
+                <div className="right-hero-section">
+                    <img src={cube} alt="3dCube" className='rh-img'/>
+                </div>
+            </section>
+            <section className='offer-section'>
+                <h1>HELLO</h1>
+            </section>
+        </>
     )
 }
 
